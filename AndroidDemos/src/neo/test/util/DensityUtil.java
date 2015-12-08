86c21f0a184e09954decf6e0411d913d889723d6
cylinzhao <keyoun@163.com>
2015-12-09 00:55:59 +0800

    add test()

--- AndroidDemos/src/neo/test/util/DensityUtil.java
+++ AndroidDemos/src/neo/test/util/DensityUtil.java
@@ -19,4 +19,8 @@ public class DensityUtil {
 		final float scale = context.getResources().getDisplayMetrics().density;
 		return (int) (pxValue / scale + 0.5f);
 	}
+	
+	public void test() {
+		
+	}
 }
